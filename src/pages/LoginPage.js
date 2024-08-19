0f440443613cee17bf2f607c39cc4b9111d9cb98
Hyeonin Jeong <bpx2710@gmail.com>
2024-08-19 16:06:03 +0900

    Modify for token

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -22,7 +22,8 @@ const LoginPage = () => {
 
       if (response.data === "아이디 또는 비밀번호가 잘못되었습니다.") {
       } else {
-        login(response.data);
+        login(response.data.userId);
+        localStorage.setItem("token", response.data.token);
         navigate("/");
       }
 
